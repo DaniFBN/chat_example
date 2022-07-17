700e02b8884de1aaef748b670f81f13eaa4818b0
Daniel <Dani_FBN@outlook.com.br>
2022-07-17 20:26:14 -0300

    feat chat card widget

--- chat_ui/lib/chat_ui.dart
+++ chat_ui/lib/chat_ui.dart
@@ -3,5 +3,6 @@ library chat_ui;
 export 'src/theme/dw_theme.dart';
 export 'src/widgets/app_bar/app_bar_home_widget.dart';
 export 'src/widgets/app_bar/app_bar_message_widget.dart';
+export 'src/widgets/chat/chat_card_widget.dart';
 export 'src/widgets/chat/chat_menu_item_widget.dart';
 export 'src/widgets/status/status_item_widget.dart';
