4fe4bd18eea63c4d3a67aa794d3b6d0f956a437f
Daniel <Dani_FBN@outlook.com.br>
2022-07-17 21:17:28 -0300

    feat message widget

---
+++ chat_ui/lib/src/widgets/message/message_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../theme/interfaces/i_dw_theme_data.dart';
+import '../text/responsive_text_widget.dart';
+
+class DwMessageWidget extends StatelessWidget {
+  const DwMessageWidget({
+    super.key,
+    required this.content,
+    required this.isMyMessage,
+  });
+
+  final String content;
+  final bool isMyMessage;
+
+  Color _handleBackgroundColor(BuildContext context) {
+    final theme = Theme.of(context).extension<IDwThemeData>()!;
+
+    if (isMyMessage) return theme.colorTheme.backgroundMyMessageColor;
+    return theme.colorTheme.backgroundTheirMessageColor;
+  }
+
+  Alignment _handleAlignment() {
+    if (isMyMessage) return Alignment.centerRight;
+    return Alignment.centerLeft;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final theme = Theme.of(context).extension<IDwThemeData>()!;
+
+    return Align(
+      alignment: _handleAlignment(),
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: size.width * theme.responsiveTheme.s16Percent,
+          vertical: size.width * theme.responsiveTheme.s8Percent,
+        ),
+        child: Container(
+          constraints: BoxConstraints(maxWidth: size.width * 0.75),
+          padding: EdgeInsets.symmetric(
+            horizontal: size.width * theme.responsiveTheme.s16Percent,
+            vertical: size.width * theme.responsiveTheme.s12Percent,
+          ),
+          decoration: BoxDecoration(
+            color: _handleBackgroundColor(context),
+            borderRadius: BorderRadius.circular(
+              size.width * theme.responsiveTheme.s16Percent,
+            ),
+          ),
+          child: DwResponsiveTextWidget(
+            text: content,
+            style: theme.textStyleTheme.messageContentTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- chat_ui/lib/src/widgets/text/responsive_text_widget.dart
+++ chat_ui/lib/src/widgets/text/responsive_text_widget.dart
@@ -7,16 +7,18 @@ class DwResponsiveTextWidget extends StatelessWidget {
     super.key,
     required this.text,
     required this.style,
+    this.overflow,
   });
 
   final String text;
+  final TextOverflow? overflow;
   final TextStyle style;
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      overflow: TextOverflow.ellipsis,
+      overflow: overflow,
       style: style.copyWith(
         fontSize: MediaQuery.of(context).widgetScale(style.fontSize ?? 14),
       ),
